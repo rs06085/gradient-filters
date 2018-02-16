97db419327da2ce00cb5d1cb5e85d9e92557dffc
User <karolemeaschris@yahoo.gr>
2018-02-16 21:58:07 +0200

    Style updated

--- Ridemind-test/Controller/MenuVC.swift
+++ Ridemind-test/Controller/MenuVC.swift
@@ -9,8 +9,20 @@ import UIKit
 
 class MenuVC: UIViewController {
 
+    @IBOutlet weak var profileImg: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         self.revealViewController().rearViewRevealWidth = self.view.frame.size.width - 150
+        profileImg.setRounded()
+    }
+}
+
+extension UIImageView {
+    
+    func setRounded() {
+        let radius = self.frame.height / 2
+        self.layer.cornerRadius = radius
+        self.layer.masksToBounds = true
     }
 }
